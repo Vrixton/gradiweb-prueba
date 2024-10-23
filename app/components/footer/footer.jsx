c797f147413628504872499f21b73c358d6ac54e
Victor Villavicencio <victor.villavicencio.10@gmail.com>
2024-10-23 07:05:49 -0500

    footer style

---
+++ app/components/footer/footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer__top"></div>
+            <h1 className="footer__title">Join the green revolution without commitment</h1>
+            <p className="footer__description"> If you are missing something and don't want to miss future promotions  or our future products</p>
+            <div className="footer--cont-form">
+                <input type="text" />
+                <button>send</button>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
+
+
