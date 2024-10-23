a782433b3e73dd8f7adaab11dc8f5105cc971642
Victor Villavicencio <victor.villavicencio.10@gmail.com>
2024-10-23 02:10:00 -0500

    header title

---
+++ app/components/header-title/header-title.jsx
@@ -0,0 +1,10 @@
+const HeaderTitle = () => (
+    <div className="cont-title">
+        <div className="badge blue-badge"></div>
+        <h1 className="title">Discover our <br /> planet pet-friendly offer</h1>
+    </div>
+)
+
+export default HeaderTitle
+
+
